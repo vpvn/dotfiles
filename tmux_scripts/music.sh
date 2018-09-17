89b071592c44a3bb5d20721b3d8a97a299825cc8
vpvn <a@vpvn.ru>
2018-09-17 15:46:30 +0300

    Fix long song title panel

--- tmux_scripts/music.sh
+++ tmux_scripts/music.sh
@@ -1,3 +1,18 @@
 #!/bin/bash
 
-mpc status -f "[%title%]" | head -n 1
+output_num=$(mpc status -f "♫ [%title%]" | wc -l)
+if (($output_num>1))
+then
+    output_str=$(mpc status -f "♫ [%title%]" | head -n 1)
+else
+    output_str="♫ --- ♫"
+fi
+
+str_len=${#output_str}
+
+if (($str_len>35))
+then
+    output_str=${output_str:0:35}'…'
+fi
+
+echo $output_str
