0aa4668605acc308b72cd95aa1d5acde393e7394
vpvn <a@vpvn.ru>
2019-05-15 18:28:08 +0300

    Update music.sh script.

--- tmux_scripts/music.sh
+++ tmux_scripts/music.sh
@@ -1,14 +1,25 @@
 #!/bin/bash
 
+# This need spotify-cli-linux package
+# to install type
+# sudo pip install spotify-cli-linux
+
 output_num=$(mpc status -f "♫ [%title%]" | wc -l)
 if (($output_num>1))
 then
     output_str=$(mpc status -f "♫ [%title%]" | head -n 1)
 else
-    output_str=$(spotify-now -i "♫ %artist - %title" -e "♫ --- ♫")
+    output_str=$(spotifycli --status)
 fi
 
 str_len=${#output_str}
+
+if (($str_len<1))
+then
+    output_str='♫ --- ♫'
+else
+    output_str='♫ '$output_str' ♫'
+fi
 
 if (($str_len>35))
 then
