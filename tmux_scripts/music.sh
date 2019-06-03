4aa466090a09159e5af68832c567284a351965cf
vpvn <a@vpvn.ru>
2019-06-03 17:03:31 +0300

    Migrate to tmux 2.9; Fix music script.

--- tmux_scripts/music.sh
+++ tmux_scripts/music.sh
@@ -1,15 +1,24 @@
 #!/bin/bash
+LC_TYPE=ru_RU.UTF-8
 
 # This need spotify-cli-linux package
 # to install type
 # sudo pip install spotify-cli-linux
+#
+# sometimes you need to fix stotifycli.py:
+#     def show_status():
+#         artist, title = get_song()
+#         print(("%s - %s" % (artist, title)).encode('utf-8'))
 
 output_num=$(mpc status -f "♫ [%title%]" | wc -l)
+play_status=''
+
 if (($output_num>1))
 then
     output_str=$(mpc status -f "♫ [%title%]" | head -n 1)
 else
     output_str=$(spotifycli --status)
+    play_status=' '$(spotifycli --playbackstatus)' '
 fi
 
 str_len=${#output_str}
@@ -18,7 +27,7 @@ if (($str_len<1))
 then
     output_str='♫ --- ♫'
 else
-    output_str='♫ '$output_str' ♫'
+    output_str=$play_status'♫ '$output_str' ♫'
 fi
 
 if (($str_len>35))
