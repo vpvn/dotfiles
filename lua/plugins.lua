b9faabdc0a4b13d1c11c15e50af276cdcf7abd23
vpvn <a@vpvn.ru>
2021-11-11 15:26:12 +0300

    Move to coc autocomplit

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -46,21 +46,23 @@ return require('packer').startup(function()
     -----------------------------------------------------------
     -- LSP и автодополнялка
     -----------------------------------------------------------
+                                
+    use {'neoclide/coc.nvim', branch = 'release'}
 
     -- Highlight, edit, and navigate code using a fast incremental parsing library
     use 'nvim-treesitter/nvim-treesitter'
     -- Collection of configurations for built-in LSP client
-    use 'neovim/nvim-lspconfig'
-    use 'williamboman/nvim-lsp-installer'
+    -- use 'neovim/nvim-lspconfig'
+    -- use 'williamboman/nvim-lsp-installer'
     -- Автодополнялка
-    use 'hrsh7th/nvim-cmp'
-    use 'hrsh7th/cmp-nvim-lsp'
-    use 'hrsh7th/cmp-buffer'
-    use 'saadparwaiz1/cmp_luasnip'
+    -- use 'hrsh7th/nvim-cmp'
+    -- use 'hrsh7th/cmp-nvim-lsp'
+    -- use 'hrsh7th/cmp-buffer'
+    -- use 'saadparwaiz1/cmp_luasnip'
     --- Автодополнлялка к файловой системе
-    use 'hrsh7th/cmp-path'
+    -- use 'hrsh7th/cmp-path'
     -- Snippets plugin
-    use 'L3MON4D3/LuaSnip'
+    -- use 'L3MON4D3/LuaSnip'
 
     -----------------------------------------------------------
     -- PYTHON
--- lua/settings.lua
+++ lua/settings.lua
@@ -76,58 +76,58 @@ augroup end
 -- Установки для плагинов
 -----------------------------------------------------------
 -- LSP settings
-local lsp_installer = require("nvim-lsp-installer")
-lsp_installer.on_server_ready(function(server)
-    local opts = {}
-    if server.name == "sumneko_lua" then
-        -- only apply these settings for the "sumneko_lua" server
-        opts.settings = {
-            Lua = {
-                diagnostics = {
-                    -- Get the language server to recognize the 'vim', 'use' global
-                    globals = {'vim', 'use'},
-                },
-                workspace = {
-                    -- Make the server aware of Neovim runtime files
-                    library = vim.api.nvim_get_runtime_file("", true),
-                },
-                -- Do not send telemetry data containing a randomized but unique identifier
-                telemetry = {
-                    enable = false,
-                },
-            },
-        }
-    end
-    server:setup(opts)
-end)
+--local lsp_installer = require("nvim-lsp-installer")
+--lsp_installer.on_server_ready(function(server)
+--    local opts = {}
+--    if server.name == "sumneko_lua" then
+--        -- only apply these settings for the "sumneko_lua" server
+--        opts.settings = {
+--           Lua = {
+--                diagnostics = {
+--                    -- Get the language server to recognize the 'vim', 'use' global
+--                    globals = {'vim', 'use'},
+--                },
+--                workspace = {
+--                    -- Make the server aware of Neovim runtime files
+--                    library = vim.api.nvim_get_runtime_file("", true),
+--                },
+--                -- Do not send telemetry data containing a randomized but unique identifier
+--                telemetry = {
+--                    enable = false,
+--                },
+--            },
+--        }
+--    end
+--    server:setup(opts)
+--end)
 
 
 -- nvim-cmp supports additional completion capabilities
-local capabilities = vim.lsp.protocol.make_client_capabilities()
-capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
-vim.o.completeopt = 'menuone,noselect'
+--local capabilities = vim.lsp.protocol.make_client_capabilities()
+--capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
+--vim.o.completeopt = 'menuone,noselect'
 -- luasnip setup
-local luasnip = require 'luasnip'
+--local luasnip = require 'luasnip'
 -- nvim-cmp setup
-local cmp = require 'cmp'
-cmp.setup {
-    snippet = {
-        expand = function(args)
-            luasnip.lsp_expand(args.body)
-        end,
-    },
-    mapping = {
-        ['<CR>'] = cmp.mapping.confirm({ select = true }),
-    },
-    sources = {
-        { name = 'nvim_lsp' },
-        { name = 'luasnip' },
-        { name = 'path' },
-        { name = 'buffer', opts = {
-            get_bufnrs = function()
-                return vim.api.nvim_list_bufs()
-            end
-        },
-    },
-},
-}
+--local cmp = require 'cmp'
+--cmp.setup {
+--    snippet = {
+--        expand = function(args)
+--            luasnip.lsp_expand(args.body)
+--        end,
+--    },
+--    mapping = {
+--        ['<CR>'] = cmp.mapping.confirm({ select = true }),
+--    },
+--    sources = {
+--        { name = 'nvim_lsp' },
+--        { name = 'luasnip' },
+--        { name = 'path' },
+--        { name = 'buffer', opts = {
+--            get_bufnrs = function()
+--                return vim.api.nvim_list_bufs()
+--            end
+--        },
+--    },
+--},
+--}
